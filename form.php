49ec2023be5835a27433c62746beb31de37fe59c
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-06-10 17:17:30 +0200

    first commit

---
+++ form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+    <div class="form_container">
+        <form action="index.php" method="GET">
+
+            <label for="nome">Inserisci un testo molto lungo</label><br>
+            <input type="text" placeholder="Text here..." name="testo"><br><br>
+
+
+            <button type="submit">Invia</button>
+
+        </form>
+    </div>
+    
+</body>
+</html>
